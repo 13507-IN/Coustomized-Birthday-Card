cf1bc499138ba0edfbbbf3c05383993d0e21f5c0
Rishiraj <rishirajnatj@gmail.com>
2026-02-19 21:25:46 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
